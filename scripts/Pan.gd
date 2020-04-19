b3b78bb54e2972d710c305634fb2b2f07459e767
gagdiez <gagdiez.c@gmail.com>
2020-04-19 19:12:32 +0200

    Changed to FSM, removed keyboard
    This is a big refactory of the code, but it was hugly needed. The main problem
    so far was that each action was a function, therefore, adding actions
    translated into adding functions. At some point, the functions had to control
    too many things:
    
    take -> walk to the object
    	look at it
    	animate the arm rising
    	put it in the inventory
    	animate the arm going down
    
    Writing the commands (walk, take, look) in such a way that they did not interfere
    with each other was growing too much in complexity, also, it was hard to read. So,
    I changed to the following:
    
    Each character (so far Cole, the only character that exists) has a queue of
    actions. Each command (walk, take, look, ...) gets translated into a set of
    actions. The character constantly looks at its queue, and goes making the
    actions one by one. In this way, take gets literally translated to the actions
    
    take -> walk, face(object), animation(rise_arm), take(), animate(lower_arm)
    
    This will make much much easier to write commands, and to add more characters
    to the game. In the way everything is writen, you can literally control two or
    more characters (of course, giving orders one at the time!).

--- scripts/Pan.gd
+++ scripts/Pan.gd
@@ -2,3 +2,4 @@ extends 'Interactive.gd'
 
 func _ready():
 	takeable = true
+	position = self.transform.origin + Vector3(3, 0, 0)
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,78 +4,56 @@ extends KinematicBody
 var DEBUG = false
 
 # Get nodes from the scene
-onready var fsm_animation = $AnimationTree.get("parameters/playback")
-onready var sprite = $cole_sprite
+onready var FSM = load("res://scripts/FSM.gd")
 onready var navigation = $Navigation
 onready var talk_bubble = $"Talk Bubble"
-onready var talk_bubble_timer = get_node("Talk Bubble/talk_bubble_timer")
+onready var talk_bubble_timer = get_node("Talk Bubble/Timer")
 
-# Properties of our player
-var current_velocity = Vector3(0, 0, 0) # so we know its current speed
-var object_to_take
+# Lets model our character as a set of actions. This will simplify the logic
+# a lot. In this way, if the character is 
+onready var queue = FSM.Queue.new()
 
+# Player variables
 const SPEED = 5
 const MINIMUM_DISTANCE = 0.5
-
-# Variables for pathfinding -> The path our player has to follow
-var path_idx = 0
-var path = []
-
-# Inventory
 var inventory = []
 
 
 func _ready():
-		talk_bubble_timer.connect("timeout", self, "quiet")
+	talk_bubble_timer.connect("timeout", self, "quiet")
 
 
+func animate(action, direction):
+	if direction:
+		if direction.x < 0:
+			$Sprite.scale.x = -1
+		else:
+			$Sprite.scale.x = 1
 
-
-
-func animate_player():
-	# If the player has velocity, then it should be walking
-	if current_velocity.x > 0:
-		sprite.scale.x = 1
-	elif current_velocity.x < 0:
-		sprite.scale.x = -1
-
-	var movement = current_velocity.x != 0 or current_velocity.z != 0
-	
-	if movement:
-		fsm_animation.travel("walk")
-	else:
-		fsm_animation.travel("idle")
-
-
-func arrived():
-	return path_idx >= path.size() and current_velocity == Vector3(0, 0, 0)
-
-
-func reset():
-	object_to_take = null
+	$Animations.play(action)
 
 
 func walk_to(object):
-	# Walk to an object
-	reset() # Override other functions
-
-	var begin = navigation.get_closest_point(self.transform.origin)
-	var end = navigation.get_closest_point(object.position)
-	
-	if (end - self.transform.origin).length() > MINIMUM_DISTANCE:
-		# We actually need to walk
-		path = navigation.get_simple_path(begin, end, true)
-		path_idx = 0
-		
-		if DEBUG:
-			$Draw.draw_path(path)
+	# Add to the queue that we want to walk
+	queue.clear()
+	queue.append(FSM.Walk.new(self, object, navigation))
 
 
 func take(object):
 	# First of all, walk to the object
-	walk_to(object)
-	
-	object_to_take = object
+	queue.clear()
+	queue.append(FSM.Walk.new(self, object, navigation))
+	queue.append(FSM.FaceObject.new(self, object))
+	queue.append(FSM.PlayAnimation.new(self, $Animations, 'take_raise'))
+	queue.append(FSM.Take.new(self, object))
+	queue.append(FSM.PlayAnimation.new(self, $Animations, 'take_down'))
+
+
+func face_object(object):
+	if object.transform.origin.x < transform.origin.x:
+		$Sprite.scale.x = -1
+	else:
+		$Sprite.scale.x = 1
 
 
 func look(object):
@@ -93,77 +71,8 @@ func say(text):
 	talk_bubble_timer.start()
 
 
-func check_if_should_take_something():
-	if object_to_take:
-		# Add object
-		inventory.append(object_to_take)
-		
-		say("I took the " + str(object_to_take.name).to_lower())
-		
-		# Tell the object you took it
-		object_to_take.take()
-		
-		# Set that you took the object
-		object_to_take = null
-
-
-func key_input_velocity():
-	# Function to control Cole using the keyboard
-	var direction = Vector3(0, 0, 0)
-	var keyboard_input = false
-	
-	if Input.is_action_pressed("ui_left"):
-		direction.x = -1
-		keyboard_input = true
-	if Input.is_action_pressed("ui_right"):
-		direction.x = 1
-		keyboard_input = true
-	if Input.is_action_pressed("ui_down"):
-		direction.z = 1
-		keyboard_input = true
-	if Input.is_action_pressed("ui_up"):
-		direction.z = -1
-		keyboard_input = true
-	
-	if keyboard_input:
-		# Clear whatever we did with the mouse
-		reset()
-
-		# Modify current velocity
-		current_velocity = direction.normalized() * SPEED
-
-
-func path_input_velocity():
-	# If you point and clicked, a path was created to be walked
-	var move_vec = Vector3(0, 0, 0)
-	
-	if path_idx < path.size():
-		# There is still path to walk		
-		move_vec = (path[path_idx] - transform.origin)
-		
-		if move_vec.length() < MINIMUM_DISTANCE:
-			# too short to walk, move to next segment
-			path_idx += 1
-			
-			# Call youself again now, so animation doesn't stutter
-			path_input_velocity()
-
-	current_velocity = move_vec.normalized() * SPEED
-
-
 func _physics_process(delta):
-	# Check if a point and click created a path
-	path_input_velocity()
+	var current_action = queue.current()
 
-	# Check if it's being controled with the keyboard
-	key_input_velocity()
-
-	# Animate the player
-	animate_player()
-	
-	# Move it
-	move_and_slide(current_velocity)
-	
-	# If not moving, check if it should be taking something
-	if arrived():
-		check_if_should_take_something()
+	if current_action:
+		current_action.run()
