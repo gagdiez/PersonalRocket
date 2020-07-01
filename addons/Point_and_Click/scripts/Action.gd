d689ddfba8edfc0deb762b3158f842b942c72f9c
gagdiez <gagdiez.c@gmail.com>
2020-07-02 01:22:26 +0200

    Added class Action

---
+++ addons/Point_and_Click/scripts/Action.gd
@@ -0,0 +1,33 @@
+class_name Action
+
+const IMMEDIATE = 'immediate'
+const TO_COMBINE = 'to_combine'
+const COMBINED = 'combined'
+
+var function
+var text
+var orig_text
+var type
+var orig_type
+var object
+var nexus = ""
+
+
+func _init(_func: String, _text:String, _type: String,
+		   _nexus: String = ""):
+	function = _func
+	text = _text
+	orig_text = _text
+	orig_type = _type
+	type = _type
+	nexus = _nexus
+
+func combine(obj):
+	object = obj
+	text = orig_text + " " + obj.oname + " " + nexus
+	type = COMBINED
+
+func uncombine():
+	object = null
+	text = orig_text
+	type = orig_type
--- addons/Point_and_Click/scripts/Actions.gd
+++ addons/Point_and_Click/scripts/Actions.gd
@@ -7,52 +7,19 @@
 #  - a "object" if the action is "combined", this is the object to combine"
 #  - a "nexus" for the text in "combined" actions (i.e. use <this> "with" <that>)
 
-const IMMEDIATE = 'immediate'
-const TO_COMBINE = 'to_combine'
-const COMBINED = 'combined'
 
-class Action:
-	var function
-	var text
-	var orig_text
-	var type
-	var orig_type
-	var object
-	var nexus = ""
-	
-	
-	func _init(_func: String, _text:String, _type: String,
-			   _nexus: String = ""):
-		function = _func
-		text = _text
-		orig_text = _text
-		orig_type = _type
-		type = _type
-		nexus = _nexus
-	
-	func combine(obj):
-		object = obj
-		text = orig_text + " " + obj.oname + " " + nexus
-		type = COMBINED
-	
-	func uncombine():
-		object = null
-		text = orig_text
-		type = orig_type
-
-
-var walk_to = Action.new("walk_to", " ", IMMEDIATE)
-var take = Action.new("take", "Take", IMMEDIATE)
-var go_to = Action.new("go_to", "Go to", IMMEDIATE)
-var read = Action.new("read", "Read", IMMEDIATE)
-var examine = Action.new("examine", "Examine", IMMEDIATE)
-var search = Action.new("search", "Search", IMMEDIATE)
-var use = Action.new("use", "Use", IMMEDIATE)
-var open = Action.new("open", "Open", IMMEDIATE)
-var talk_to = Action.new("talk_to", "Talk to", IMMEDIATE)
-var use_item = Action.new("use_item", "Use", TO_COMBINE, 'with')
-var say = Action.new("say", "", IMMEDIATE)
+var walk_to = Action.new("walk_to", " ", Action.IMMEDIATE)
+var take = Action.new("take", "Take", Action.IMMEDIATE)
+var go_to = Action.new("go_to", "Go to", Action.IMMEDIATE)
+var read = Action.new("read", "Read", Action.IMMEDIATE)
+var examine = Action.new("examine", "Examine", Action.IMMEDIATE)
+var search = Action.new("search", "Search", Action.IMMEDIATE)
+var use = Action.new("use", "Use", Action.IMMEDIATE)
+var open = Action.new("open", "Open", Action.IMMEDIATE)
+var talk_to = Action.new("talk_to", "Talk to", Action.IMMEDIATE)
+var use_item = Action.new("use_item", "Use", Action.TO_COMBINE, 'with')
+var say = Action.new("say", "", Action.IMMEDIATE)
 
 
 # The empty action
-var none = Action.new("", "", IMMEDIATE)
+var none = Action.new("", "", Action.IMMEDIATE)
